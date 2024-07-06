c73768d5a3245282986c5f4215d7a03f3a8efbad
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-07-06 19:00:20 +0400

    :bug: Uncondenses some whitespace
    for clarity

--- index.js
+++ index.js
@@ -31,60 +31,78 @@ const MINUTE = 60000,
 
 const randomPhrase = () => phrases[(Math.random() * phrases.length) | 0];
 
+
 const h = (tag, props, ...children) => {
   const node = Object.assign(document.createElement(tag), props);
   node.append(...children);
   return node;
-};
+}
 //------------------------------------ SELECT OPTIONS --------------------------------------------
-const renderOption = (value, textContent) => h('option', { value }, textContent);
+const renderOption = (value, textContent) =>
+  h('option', { value }, textContent);
 
-const renderSelect = (grave) => filterSelect.append(renderOption('any', `All (${grave.length})`), ...Object.entries(Object.groupBy(grave, (item) => item.type)).map(([type, items]) => renderOption(type, `${type} (${items.length})`)));
+const renderSelect = (grave) => 
+  filterSelect.append(renderOption('any', `All (${grave.length})`), ...Object.entries(Object.groupBy(grave, (item) => item.type)).map(([type, items]) => renderOption(type, `${type} (${items.length})`)));
+
 
 //----------------------------------------- GRID -------------------------------------------------
 const relative = new Intl.RelativeTimeFormat('en', { numeric: 'auto' });
 const renderGrid = (grave, keyword = '', type = 'any') => {
   const graveStones = grave
-    .filter((stone) => (stone.name.toLowerCase().includes(keyword) || stone.description.toLowerCase().includes(keyword)) && (type === 'any' || stone.type === type))
-    .map((stone) => {
+    .filter((stone) => 
+      (stone.name.toLowerCase().includes(keyword) || stone.description.toLowerCase().includes(keyword)) 
+      && (type === 'any' || stone.type === type)
+    ).map((stone) => {
       const itemDate = new Date(stone.dateClose);
       const itemAge = humanDistance(new Date(stone.dateOpen), itemDate);
       const isFuture = itemDate > Date.now();
       const killedText = isFuture ? `${randomPhrase()} ${relative.format(...itemAge)}` : `Killed about ${relative.format(...itemAge)}`;
-      return h(
-        'article',
-        null,
-        h(
-          'div',
-          null,
-          h('img', {
-            height: 50,
-            src: `img/${isFuture ? 'guillotine.svg' : 'tombstone.svg'}`,
-            alt: ''
-          }),
-          h('time', null, isFuture ? itemDate.toLocaleString('en-US', { month: 'long', year: 'numeric' }).replace(' ', '\n') : `${stone.dateOpen.split('-')[0]} - ${stone.dateClose.split('-')[0]}`),
-          h('div', null, stone.type)
-        ),
-        h('div', null, h('h2', null, h('a', { href: stone.link, target: '_blank' }, stone.name)), h('div', null, `${killedText}, ${stone.description}. ${isFuture ? 'It will be' : 'It was'} ${Math.abs(itemAge[0])} ${itemAge[1]}${Math.abs(itemAge[0]) === 1 ? '' : 's'} old.`))
-      );
-    });
+        return (
+          h('article', null,
+            h('div', null, 
+              h('img', { 
+                height: 50, 
+                src: `img/${isFuture ? 'guillotine.svg' : 'tombstone.svg'}`, 
+                alt: ''
+              }), 
+              h('time', null, 
+                isFuture 
+                ? itemDate.toLocaleString('en-US', { month: 'long', year: 'numeric' }).replace(' ', '\n')
+                : `${stone.dateOpen.split('-')[0]} - ${stone.dateClose.split('-')[0]}`
+              ), 
+              h('div', null, stone.type)
+            ),
+            h('div', null,
+              h('h2', null, 
+                h('a', { href: stone.link, target: '_blank'}, stone.name)
+              ),
+              h('div', null, 
+                `${killedText}, ${stone.description}. ${isFuture ? 'It will be' : 'It was'} ${Math.abs(itemAge[0])} ${itemAge[1]}${Math.abs(itemAge[0]) === 1 ? '' : 's'} old.`)
+            )
+          )
+        )
+    })
 
   gridCont.replaceChildren(h('aside'), ...graveStones);
-};
+}
 //------------------------------------------------------------------------------------------------
+
 
 const changeFilter = (cacheData) => {
   const keyword = searchInput.value.toLowerCase();
   const type = filterSelect.value;
   renderGrid(cacheData, keyword, type);
-};
+}
 
 const humanDistance = (dateCompare) => {
   const delta = dateCompare.getTime() - Date.now();
-  if (Math.abs(delta) < MONTH) return [(delta / DAY) | 0, 'day'];
-  if (Math.abs(delta) < YEAR) return [(delta / MONTH) | 0, 'month'];
-  return [(delta / YEAR) | 0, 'year'];
-};
+  if (Math.abs(delta) < MONTH)
+    return [delta / DAY | 0, 'day'];
+  if (Math.abs(delta) < YEAR)
+    return [delta / MONTH | 0, 'month'];
+  return [delta / YEAR | 0, 'year'];
+}
+
 
 const res = await fetch('data/graveyard.json');
 
